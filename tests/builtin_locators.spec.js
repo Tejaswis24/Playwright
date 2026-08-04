b66fbc9109b2f6838b65f5af528cda365bbed6f5
Tejaswi <tejaswis489@gmail.com>
2026-08-04 23:46:20 +0530

    commit

---
+++ tests/builtin_locators.spec.js
@@ -0,0 +1,34 @@
+/*
+page.getbyalttext()----to locate an element usually image, by its text alternative(To check the logo)
+page.getbyplaceholder()----to locate an input by placeholder
+page.getbyrole()----to locate by explicit and implicit accesbility attributes
+page.getbytext()----to locate by test content
+
+these below 3 refer play wright document 
+page.getbylabel()----to locate form control by associated label text
+page.getbytitle()----by its title attribute
+page.getbytestid()----based on data test id attribute
+*/
+
+
+import {test, expect} from '@playwright/test'
+test('builtin_locators',async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    //to check the logo is present/not
+    const logo = await page.getByAltText('company-branding')
+    await expect(logo).toBeVisible();
+
+    //get by placeholder
+    await page.getByPlaceholder('Username').fill('Admin')
+    await page.getByPlaceholder('Password').fill('admin123')
+
+    //get by role here button is the role, other like  select, link
+    await page.getByRole('button',{type:'submit'}).click()
+
+    //get by text here 27 line is fetching the name and its stored in name variable
+   const name=await page.locator('//p[@class="oxd-userdropdown-name"]').textContent
+    await expect(await page.getByText(name)).toBeVisible()
+
+    //
+
+})
